554429c58b80189bb075f90f852a7d292c6d8438
Jakub Zgolinski <jakub@zgolinski.net>
2019-12-03 22:30:05 +0100

    day3

--- day3/prog.py
+++ day3/prog.py
@@ -3,22 +3,16 @@ from collections import defaultdict
 with open('input.txt') as f:
     a, b = [x.strip().split(",") for x in f.readlines()]
 
-# a = "R8,U5,L5,D3".split(",")
-# b = "U7,R6,D4,L4".split(",")
-
-# a = "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51".split(",")
-# b = "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7".split(",")
-
-# a = "R75,D30,R83,U83,L12,D49,R71,U7,L72".split(",")
-# b = "U62,R66,U55,R34,D71,R55,D58,R83".split(",")
-
 def plot(source, name):
     x, y = 0, 0
+    l = 0
     global grid
+    global pedo
     for path in source:
         dir = path[0]
         steps = int(path[1:])
         for _ in range(steps):
+            l = l + 1
             if dir == "R":
                 x = x + 1
             if dir == "L":
@@ -28,19 +22,22 @@ def plot(source, name):
             if dir == "D":
                 y = y - 1
             grid[(x, y)].add(name)
+            pedo[(x, y)].add(l)
 
 grid = defaultdict(set)
+pedo = defaultdict(set)
 
 plot(a, "a")
 plot(b, "b")
 
 crossings = []
+lengths = []
 for coord in grid:
     if len(grid[coord]) >= 2:
         crossings.append(coord)
+        lengths.append(pedo[coord])
 
+steps_combined = [sum(x) for x in lengths]
 distances = [abs(x[0]) + abs(x[1]) for x in crossings]
 
-print(crossings, distances)
-
-print(sorted(distances)[0])
+print(f"Shortest distance: {sorted(distances)[0]}\nFewest steps: {sorted(steps_combined)[0]}")

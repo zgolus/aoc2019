da9ae23ca636bd6f0f4735c50969f05098d69c4a
Jakub Zgolinski <jakub@zgolinski.net>
2019-12-07 01:08:36 +0100

    day 6, part 1

--- day6/prog.py
+++ day6/prog.py
@@ -8,29 +8,13 @@ def parse_orbit_data(data: str):
     orbits[orbiting].append(orbited)
     indirect = orbits[orbited]
     orbits[orbiting].extend(indirect)
+    [orbits[x].extend(indirect + [orbited]) for x, y in orbits.items() if orbiting in y]
 
-with open("day6/input_test.txt") as f:
+with open("day6/input.txt") as f:
     [parse_orbit_data(x.strip()) for x in f.readlines()]
 
 cnt = 0
 for o in orbits:
     cnt = cnt + len(orbits[o])
 
-print(cnt, orbits)
-
-# COM)B
-# B)C
-# C)D
-# D)E
-# E)F
-# B)G
-# G)H
-# D)I
-# E)J
-# J)K
-# K)L
-#         G - H       J - K - L
-#        /           /
-# COM - B - C - D - E - F
-#                \
-#                 I
+print(cnt)
